9ab4bb3dd6c209c8397b1e02a587ee021c5b88b1
Gabriel Rondon <gabrielbarragan07@gmail.com>
2022-05-23 13:36:09 -0500

    todos los métodos terminados

--- array_methods.rb
+++ array_methods.rb
@@ -13,6 +13,7 @@ if __FILE__ == $PROGRAM_NAME
         limit_left = n
         
         no_drop_elements = array.slice(limit_left..-1)
+        #no_drop_elements = array.drop(n)
         
         return no_drop_elements
 
@@ -46,8 +47,38 @@ if __FILE__ == $PROGRAM_NAME
 
         return new_array
     end
-    
 
+    def index_arr_multiple_add(array, pos, *elements)
+        
+        new_array= array.insert(pos, *elements)
+
+        return new_array
+    end
+
+    def end_arr_delete(array)
+        
+        new_array= array.last
+
+        return new_array
+    end
+
+    def start_arr_delete(array)
+        new_array = array.first
+        
+        return new_array
+    end
+    def delete_at_arr(array,position)
+        array.delete_at(position)
+                
+        return array
+    end
+
+    def delete_all (array, position)
+
+        array.delete(6)
+        return array
+
+    end
 
 #driver code
 
@@ -56,15 +87,11 @@ if __FILE__ == $PROGRAM_NAME
     p end_arr_add([1, 4, 3, 6, 5], 20) == [1, 4, 3, 6, 5, 20]
     p begin_arr_add([1, 4, 3, 6, 5], 34) == [34, 1, 4, 3, 6, 5]
     p index_arr_add([1, 4, 3, 6, 5], 2, 100) == [1, 4, 100, 3, 6, 5]
+    p index_arr_multiple_add([1, 4, 3, 6, 5], 3, 34, 23) == [1, 4, 3, 34, 23, 6, 5]
+    p end_arr_delete([1, 4, 3, 6, 5]) == 5
+    p start_arr_delete([1, 4, 3, 6, 5]) == 1
+    p delete_at_arr([1, 4, 3, 6, 5], 2) == [1, 4, 6, 5]
+    p delete_all([1, 4, 3, 6, 5], 6) == [1, 4, 3, 5]
 end
 
 
-# p index_arr_multiple_add([1, 4, 3, 6, 5], 3, 34, 23) == [1, 4, 3, 34, 23, 6, 5]
-
-# p end_arr_delete([1, 4, 3, 6, 5]) == 5
-
-# p start_arr_delete([1, 4, 3, 6, 5]) == 1
-
-# p delete_at_arr([1, 4, 3, 6, 5], 2) == [1, 4, 6, 5]
-
-# p delete_all([1, 4, 3, 6, 5], 6) == [1, 4, 3, 5]
